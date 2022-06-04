4bf1602d69bf535c32a4f041d2ae4393942585cf
selametfirdaus <selametfirdaus@gmail.com>
2022-06-04 23:12:15 +0700

    Initial Commit Add Sesi 1

---
+++ Day1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 09:50:03 2022
+
+@author: 089582
+"""
+
+print (123123+1)
---
+++ Day1_1.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 10:27:39 2022
+
+@author: 089582
+"""
+
+n=300
+print(n)
+
+a=10
+t=20
+l=a*t/2
+print(l)
+
+var = 23.5
+print(var)
+
+var = "Now I'm a string"
+print(var)
+
+name = "Hacktiv8"
+Age = 54
+has_laptops = True
+print(name, Age, has_laptops)
+
+depan = 'Muh'
+tengah = 'Selamet'
+belakang = 'Firdaus'
+print(depan, tengah, belakang)
+
+a = 10
+b = 20
+a + b * 5
+print(a+b*9)
+
+#--------------
+a = 4
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+
+#------------
+a = 10
+b = 20
+print(a == b)
+
+print(a != b)
+
+print(a <= b)
+
+print(a >= b)
+
+
+a = 30
+b = 30
+print(a == b)
+
+print(a <= b)
+
+print(a >= b)
+
+
+
+# + Operators
+s = 'foo'
+t = 'bar'
+u = 'baz'
+
+print(s + t)
+
+print(s + t + u)
+
+
+print('Hacktiv8' +' '+ 'PTP')
